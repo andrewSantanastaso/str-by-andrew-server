ecfcf84a03d4a830754714290ccefaa28c181a8b
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-10-05 20:02:51 -0400

    server rrunning

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.once('connected', () => {
+    console.log('Connected to MongoDB')
+})
+mongoose.connection.on('error', () => {
+    console.error('Error connecting to mongoDB')
+})
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
